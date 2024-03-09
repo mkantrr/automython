5233a3eea470bee1fa9e1cc48cef1f309aec3b7e
Matthew Kanter <mattkanter1@outlook.com>
2024-03-08 21:01:32 -0500

    deprecation fix and project creation updates

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ README = (HERE / "README.md").read_text()
 setup(
     name='automython',
     version='1.0.0',
-    description='',
+    description='A simplistic programming language interpreter to Python to help students grasp finite automata theory programmatically and with a computed graph through visualization libraries.',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/mkantrr/automython',
     author='Matthew Kanter',
     author_email='matt@matutu.dev',
     license='MIT',
-    keywords=['automata', 'compiler', 'interpreter', 'theory', 'computational theory'],
+    keywords=['automata', "finite", 'compiler', 'interpreter', 'theory', 'computational theory', "non-deterministic", "turing", "machine", "state"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
